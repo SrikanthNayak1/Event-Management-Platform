78ce3ef6ee891f147e1719bbf64d2a81cc888f7c
Srikanth Sabavath <118047473+SrikanthNayak1@users.noreply.github.com>
2025-02-08 21:55:13 +0530

    Add files via upload

--- Fronted/App.js
+++ Fronted/App.js
@@ -1,1 +1,24 @@
-h
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+
+function App() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        socket.on("message", (msg) => {
+            console.log(msg);
+            setMessage(msg);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>Event Management Platform</h1>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default App;
