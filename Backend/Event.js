78d13ce3c257da0514c7899491f04f3d699f9da2
Srikanth Sabavath <118047473+SrikanthNayak1@users.noreply.github.com>
2025-02-08 21:52:29 +0530

    Add files via upload

---
+++ Backend/Event.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    date: Date,
+    attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+});
+
+module.exports = mongoose.model("Event", EventSchema);
